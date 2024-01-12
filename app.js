5a7ffd2bdb9a8a8b0d146185f880a8ebcea930ad
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-12 10:29:52 +0000

    has it worked?

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Welcome to day 4 dan!");
+
+const waves = document.getElementById("waves");
+const playbtn = document.getElementById("playbtn");
+const pausebtn = document.getElementById("pausebtn");
+const stopbtn = document.getElementById("stopbtn");
+const skipfwd = document.getElementById("skipfwd");
+const skipbk = document.getElementById("skipbk");
